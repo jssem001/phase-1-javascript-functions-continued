df8535cefaae04c6767758d234cf5dd50bb3792d
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-19 20:42:56 +0300

    more functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(act = 'roller-skate'){
+    let acti = `This Saturday, I want to ${act}!`
+    return acti
+}
+//saturdayFun()
+function mondayWork(act = 'go to the office'){
+    let acti = `This Monday, I will ${act}.`
+    return acti
+}
+
+function wrapAdjective(emph="*"){
+    return function (msg= "Special"){
+        return `You are ${emph}${msg}${emph}!`
+    }
+}
+
+
+// function outer(greeting, msg = "You are") {
+//     // 2
+//     const innerFunction = function (name) {
+//       // 3
+//       let statement = `${msg} ${greeting} ${name} ${greeting}`
+//       return statement // 4
+//     }
+//     return innerFunction("some string") // 5
+//   }
+  
